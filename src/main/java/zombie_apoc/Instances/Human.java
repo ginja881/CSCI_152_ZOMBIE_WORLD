2b8246cfbe8f6c7d5304f6537466a0f454a0e993
ginja881 <plutocstars@gmail.com>
2025-11-22 13:17:40 -0700

    Maven

---
+++ src/main/java/zombie_apoc/Instances/Human.java
@@ -0,0 +1,53 @@
+/*
+ * Author Name: Joseph Carter
+ * Submission Date: 11/12/2025
+ * Description: human class representation, for abstraction of two main entities
+ */
+
+
+package Instances;
+
+import Instances.World;
+import Instances.Creature;
+import Instances.Zombie;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Human extends Creature {
+    // Reproduction rate
+	public double reproduce_rate;
+    
+	// Neighbors
+	public ArrayList<int[]> get_neighbors() { return this.neighbors;};
+	protected void update_neighbors() {
+		// TODO: Make updating algorithm
+	}
+	public void move() {
+		// TODO: Implement probabilistic moving
+	}
+	public void die() {
+		// TODO: Wrapper for World.removeCreature()
+	}
+
+	public void battle(Creature enemy) {
+		// TODO: Implement fighting mechanic for when humans are neighbored to a zombie, based off of battle_rate
+		
+	}
+
+	public Human reproduce(Human partner) {
+		// TODO: Implement probability of reproduction
+		return this;
+	}
+	// Constructor
+	public Human(World world, int x, int y, double move_rate, double reproduce_rate) {
+		this.world = world;
+		this.x = x;
+		this.y = y;
+		this.move_rate = move_rate;
+		this.reproduce_rate = reproduce_rate;
+		this.neighbors = new ArrayList<int[]>();
+		update_neighbors();
+	}
+
+}
